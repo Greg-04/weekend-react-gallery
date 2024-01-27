47f4f4713858e29f9a20cd12cdf73128a7a26ad0
Gabriel Regalado <regs0404@gmail.com>
2024-01-27 17:34:52 -0500

    passed second test of render toggle

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,18 +1,35 @@
 import './GalleryItem.css';
+import { useState } from 'react';
 
 function GalleryItem({ galleryData }) {
+  const [toggleDescription, setToggleDescription] = useState(true);
+
+  const handleToggle = () => {
+    setToggleDescription(!toggleDescription);
+  };
+
   return (
     <div data-testid="galleryItem">
       <h3>Title: {galleryData.title}</h3>
-      <div className="gallery-image">
-        <img
-          className="gallery-image"
-          src={galleryData.url}
-          alt={galleryData.title}
-        />
+      {toggleDescription ? (
+        <div className="gallery-image">
+          <img
+            className="gallery-image"
+            src={galleryData.url}
+            alt={galleryData.title}
+          />
+        </div>
+      ) : (
+        <p className="Description" data-testid="description">
+          {galleryData.description}
+        </p>
+      )}
+      <div>
+        <button data-testid="toggle" onClick={handleToggle}>
+          Click for Description!
+        </button>
+        <button data-testid="like">Love it!</button>
       </div>
-      <p data-testid="description">{galleryData.description}</p>
-      <button data-testid="like">Love it!</button>
       <div data-testid="like"> {galleryData.likes} people love this!</div>
     </div>
   );
