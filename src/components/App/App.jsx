036e477e509774bb9a3079fa6a90de84d5828c57
Gabriel Regalado <regs0404@gmail.com>
2024-01-27 15:54:44 -0500

    added gallerylist and render

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,5 @@
+import GalleryList from '../GalleryList/GalleryList';
+
 function App() {
   return (
     <div>
@@ -6,11 +8,7 @@ function App() {
       </header>
 
       <p>The gallery goes here!</p>
-      <img src="images/goat_small.jpg" />
-      <img src="images/coffee.webp" />
-      <img src="images/DallasCowboysIMG.avif" />
-      <img src="images/goldendoodle.png" />
-      <img src="images/dachsand.webp" />
+      <GalleryList data-testid="galleryList" />
     </div>
   );
 }
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './GalleryList.css';
+
+const fetchGalleryList = () => {
+  return axios.get('/api/gallery');
+};
+
+function GalleryList() {
+  const [galleryList, setGalleryList] = useState([]);
+
+  const refreshGalleryList = () => {
+    //api call
+    const listPromise = fetchGalleryList();
+    listPromise
+      //success
+      .then((response) => {
+        console.log('Server Data:', response.data);
+        setGalleryList(response.data);
+      })
+      //failure
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  //initial load of component
+  useEffect(() => {
+    //body of effect
+    console.log('Hi');
+    //api call
+    refreshGalleryList();
+  }, []);
+
+  return (
+    <main>
+      <h2>Gallery List</h2>
+      {galleryList.map((galleryData, dataIndex) => {
+        return (
+          <div key={dataIndex}>
+            <h3>Title: {galleryData.title}</h3>
+            <div>
+              <img
+                className="gallery-image"
+                src={galleryData.url}
+                alt={galleryData.title}
+              />
+            </div>
+            <p>{galleryData.description}</p>
+            <button>Love it!</button>
+            <span> {galleryData.likes} </span>
+          </div>
+        );
+      })}
+    </main>
+  );
+}
+
+export default GalleryList;
